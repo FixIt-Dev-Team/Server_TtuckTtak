5ec57b709c9623e82acae0fc6c8396d902be6589
destiny3912 <destiny3910@gmail.com>
2023-02-02 20:51:08 -0800

    Project init

---
+++ src/main/java/com/service/ttucktak/TtuckTakApplication.java
@@ -0,0 +1,13 @@
+package com.service.ttucktak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtuckTakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtuckTakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/ttucktak/TtuckTakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.ttucktak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtuckTakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
