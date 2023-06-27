506b1acac0f5f7403a1be5e1ae9f7b5600755fa8
WaGaNaWa <m4a1carbin4@gmail.com>
2023-06-27 23:58:10 +0900

    appspec.yml setting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --tag ttuckttak:0.0.1 ./ && docker run -p 6543:6543 ttuckttak:0.0.1
+cd ../ && docker build --tag ttuckttak:0.0.1 ./ && docker run -p 6543:6543 ttuckttak:0.0.1
