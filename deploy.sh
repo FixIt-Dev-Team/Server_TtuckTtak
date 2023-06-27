db18a428cfcc313916369c2f28fc446ad13e2fa9
WGNW_gangdodan <52302387+m4a1carbin4@users.noreply.github.com>
2023-06-27 22:13:00 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo screen -S Ttuckttak_dev -X quit
-sudo screen -dms Ttuckttak_dev bash -c 'sudo docker build --tag ttuckttak:0.0.1 ./ && sudo docker run -p 6543:6543 ttuckttak:0.0.1'
+screen -S Ttuckttak_dev -X quit
+screen -dms Ttuckttak_dev bash -c 'docker build --tag ttuckttak:0.0.1 ./ && docker run -p 6543:6543 ttuckttak:0.0.1'
