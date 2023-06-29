e69aa905ee74aa5611dbd153964624c355149d96
WaGaNaWa <m4a1carbin4@gmail.com>
2023-06-29 13:42:08 +0900

    deploy.sh edit

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,25 @@
 
 cd /home/ubuntu/github_action/
 
-screen -S Ttuckttak_dev -X quit
-screen -dms Ttuckttak_dev bash -c 'docker build --tag ttuckttak:0.0.1 ./ && docker run -p 6543:6543 ttuckttak:0.0.1'
+db_image_name="ttuckttak_img"
+db_container_name="ttuckttak"
+version="0.0.1"
+port=6543 # Default MySQL Port: 3306
+
+echo "## Automation docker-database build and run ##"
+
+# remove container
+echo "=> Remove previous container..."
+docker rm -f ${db_container_name}
+
+# remove image
+echo "=> Remove previous image..."
+docker rmi -f ${db_image_name}:${version}
+
+# new-build/re-build docker image
+echo "=> Build new image..."
+docker build --tag ${db_image_name}:${version}
+
+# Run container
+echo "=> Run container..."
+docker run -d -p ${port}:${port} --name ${db_container_name} ${db_image_name}:${version}
