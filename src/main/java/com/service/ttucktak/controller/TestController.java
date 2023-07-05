6829b734dd4825657982287bb1a7d298cdaae0d5
destiny3912 <destiny3910@gmail.com>
2023-07-05 16:16:03 +0900

    [FIX] Exception Advice Unexpeted Error return code

--- src/main/java/com/service/ttucktak/controller/TestController.java
+++ src/main/java/com/service/ttucktak/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.service.ttucktak.controller;
 
+import com.service.ttucktak.base.BaseErrorCode;
+import com.service.ttucktak.base.BaseException;
+import com.service.ttucktak.base.BaseResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,10 @@ public class TestController {
     @GetMapping("/health")
     public String healthTest(){
         return "OK";
+    }
+
+    @GetMapping("/exception")
+    public BaseResponse<String> exceptionTest(){
+        return new BaseResponse<>(new BaseException(BaseErrorCode.DATABASE_ERROR));
     }
 }
