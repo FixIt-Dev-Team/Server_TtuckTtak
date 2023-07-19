c2b941859dbc90360705f70fc218271df190fcac
destiny3912 <destiny3910@gmail.com>
2023-07-19 14:07:58 +0900

    [FIX] CICD

--- src/test/java/com/service/ttucktak/TtuckTakApplicationTests.java
+++ src/test/java/com/service/ttucktak/TtuckTakApplicationTests.java
@@ -1,18 +1,13 @@
 package com.service.ttucktak;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-
-import static org.junit.Assert.assertTrue;
 
 @SpringBootTest(properties = "spring.config.location= classpath:/application.yml,classpath:/secret.yml")
 class TtuckTakApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.assertTrue(true);
     }
 
 }
