70699de01e91bc598e22e5f58e77fc532e0a6f78
‘namsh1125’ <namsh1125@naver.com>
2023-07-23 05:18:07 +0900

    [FEAT] 닉네임 비속어 필터링 기능 추가

--- src/main/java/com/service/ttucktak/controller/AuthController.java
+++ src/main/java/com/service/ttucktak/controller/AuthController.java
@@ -146,7 +146,7 @@ public class AuthController {
     @GetMapping("nickname")
     public BaseResponse<GetNicknameAvailableResDto> checkNicknameAvailability(@RequestParam("nickname") String nickname) {
         try {
-            boolean isAvailable = !authService.checkNicknameExists(nickname);
+            boolean isAvailable = authService.nicknameAvailable(nickname);
             GetNicknameAvailableResDto result = new GetNicknameAvailableResDto(isAvailable);
             return new BaseResponse<>(result);
 
