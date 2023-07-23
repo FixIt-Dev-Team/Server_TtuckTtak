6ee4773da837a4573693d21341ee0ccdd4059972
WaGaNaWa <m4a1carbin4@gmail.com>
2023-07-23 15:49:41 +0900

    add logout API + PW change API moved to MemberController
    swagger document added

--- src/main/java/com/service/ttucktak/dto/auth/TokensDto.java
+++ src/main/java/com/service/ttucktak/dto/auth/TokensDto.java
@@ -1,5 +1,6 @@
 package com.service.ttucktak.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,10 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class TokensDto {
+    @Schema(name = "grantType", example = "Bearer", requiredProperties = "true", description = "토큰 타입")
     private String grantType;
+    @Schema(name = "accessToken", example = "tokenString", requiredProperties = "true", description = "서비스 엑세스 토큰 스트링")
     private String accessToken;
+    @Schema(name = "refreshToken", example = "tokenString", requiredProperties = "true", description = "서비스 리프레쉬 토큰 스트링")
     private String refreshToken;
 }
