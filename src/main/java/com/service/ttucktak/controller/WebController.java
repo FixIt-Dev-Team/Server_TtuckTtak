27fda61233c3285542609a41ff2eb053f869ddb4
WaGaNaWa <m4a1carbin4@gmail.com>
2023-09-30 01:15:50 +0900

    add main page html and some static files + fix : add license + quick fix

--- src/main/java/com/service/ttucktak/controller/WebController.java
+++ src/main/java/com/service/ttucktak/controller/WebController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -17,16 +18,16 @@ public class WebController {
      * */
     @GetMapping()
     public String mainPage(){
-        return "/index.html";
+        return "/index";
     }
 
     @GetMapping("/index")
     public String indexPage(){
-        return "/index.html";
+        return "/index";
     }
 
     @GetMapping("/contact")
     public String contactPage(){
-        return "/contact.html";
+        return "/contact";
     }
 }
